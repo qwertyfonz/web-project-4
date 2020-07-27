2908f8d530faaff0757f234618906ceceee0d80f
qwertyfonz <qwertyfonz@DESKTOP-7RAHV6L.localdomain>
2020-07-26 20:17:13 -0400

    Implemented follow, like, pagination, edit post, following functionalities

--- network/urls.py
+++ network/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 
 from . import views
@@ -8,5 +7,13 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+   
+
+    # API Routes
+    path("like", views.like, name="like"),
+    path("followingview", views.followingView, name="followingview"),
+    path("followunfollow", views.followUnfollow, name="followunfollow"),
+    path("edit", views.edit, name="edit"),
+
     path("<str:username>", views.profile, name="profile")
 ]
--- network/views.py
+++ network/views.py
@@ -1,10 +1,14 @@
+import json
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import *
 from .models import *
@@ -12,7 +16,20 @@ from .models import *
 
 def index(request):
     postForm = PostForm(request.POST)
+    allPosts = Post.objects.all().order_by("-createdDate")
 
+    # Creates pagination with 10 posts a page
+    paginator = Paginator(allPosts, 10)
+    page = request.GET.get("page")
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    # Capture values from form input and save new Post
     if request.method == "POST":
         if postForm.is_valid():
             postForm                = postForm.save(commit=False)
@@ -20,13 +37,16 @@ def index(request):
             postForm.createdDate    = datetime.now()
             postForm.numOfLikes     = 0
             postForm.save()
-            return redirect('index')
+            return redirect("index")
     else:
         postForm = PostForm()
     
+    # Render all posts page
     return render(request, "network/index.html", {
         "postForm": PostForm(),
-        "allPosts": Post.objects.all().order_by('-createdDate')
+        "page"    : page,
+        "posts"   : posts
+        
     })
 
 def login_view(request):
@@ -56,12 +76,12 @@ def logout_view(request):
 
 def register(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+        username    = request.POST["username"]
+        email       = request.POST["email"]
 
         # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
+        password        = request.POST["password"]
+        confirmation    = request.POST["confirmation"]
         if password != confirmation:
             return render(request, "network/register.html", {
                 "message": "Passwords must match."
@@ -80,20 +100,149 @@ def register(request):
     else:
         return render(request, "network/register.html")
 
+
 def profile(request, username):
     loggedInUser = request.user
-    profileName = User.objects.get(username=username) 
-    numFollowing = Following.objects.filter(user=loggedInUser).count()
-    numFollowers = Following.objects.filter(followUser=loggedInUser).count()
-    listOfMyPosts = Post.objects.filter(user=profileName).order_by('-createdDate')
+    # Get info for a profile page
+    try:
+        profileName         = User.objects.get(username=username)
+        loggedInUserID      = loggedInUser.id
+        usernameID          = profileName.id
+
+        # Check to see if user is already following a profile
+        followStatus        = len(Following.objects.filter(user=loggedInUserID, followUser=usernameID)) > 0
+    except User.DoesNotExist:
+        profileName = None
+        followStatus = False
     
-    if request.method == "POST":
-        buttonValue = request.POST.get("button-click", None)
+    # Checks for number of followers, following, and a list of the profile's posts
+    numFollowing    = Following.objects.filter(user=profileName).count()
+    numFollowers    = Following.objects.filter(followUser=profileName).count()
+    listOfMyPosts   = Post.objects.filter(user=profileName).order_by("-createdDate")
+
+    # Creates pagination with 10 posts a page
+    paginator = Paginator(listOfMyPosts, 10)
+    page = request.GET.get("page")
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, "network/profile.html", {
-        "numFollowing": numFollowing,
-        "numFollowers": numFollowers,
-        "profileName" : username,
-        "loggedInUser": request.user.username,
-        "myPosts"     : listOfMyPosts
+        "numFollowing"    : numFollowing,
+        "numFollowers"    : numFollowers,
+        "profileName"     : username,
+        "loggedInUser"    : loggedInUser,
+        "posts"           : posts,
+        "followStatus"    : followStatus,
+        "page"            : page
     })
+
+@csrf_exempt
+def like(request):
+    
+    # Get necessary parameters for a like (user, post)
+    try:
+        requestUser    = request.user
+        data           = json.loads(request.body)
+        postId         = data.get("postId")
+        post           = Post.objects.filter(id=postId).first()
+
+        # Checks to see if a user has liked a post before
+        likedPost      = Likes.objects.filter(user=requestUser, post=post)
+        ifLikeExists   = len(likedPost) > 0 
+        likeStatus     = False
+
+        # If like already exists, then unlike the post
+        if ifLikeExists:
+            post.numOfLikes = post.numOfLikes - 1
+            likedPost.delete()
+            
+
+        # If not liked yet, then like the post
+        else:
+            post.numOfLikes = post.numOfLikes + 1
+            likePost = Likes.objects.create(user=requestUser, post=post)
+            likeStatus = True
+        
+        # Save the number of likes on the post
+        post.save()
+
+    except:
+        return JsonResponse({"error": "Unable to like/unlike."}, status=404)
+    return JsonResponse({"numOfLikes": post.numOfLikes, "likeStatus": likeStatus}, status=201)
+
+
+@login_required
+def followingView(request):
+
+    # Query for all distinct followed users
+    usersFollowed = Following.objects.filter(user=request.user).values("followUser").distinct()
+
+    # Filter all Posts by those the user follows
+    followingPosts = Post.objects.filter(user__in = usersFollowed).order_by("-createdDate")
+
+    # Creates pagination with 10 posts a page
+    paginator = Paginator(followingPosts, 10)
+    page = request.GET.get("page")
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, "network/following.html", {
+        "posts" : posts,
+        "page"  : page
+    })    
+
+def followUnfollow(request):
+
+    # Get necessary variables to follow another user (follow status, username)
+    try:
+        requestUser     = request.user
+        data            = json.loads(request.body)
+        action          = data.get("action")
+        profileName     = data.get("userProfile")
+        userProfile     = User.objects.get(username=profileName)
+
+        # Checks to see if a user is already following another user
+        following       = Following.objects.filter(user=requestUser, followUser=userProfile)
+        numFollowers    = len(following)
+        isFollowing     = numFollowers  > 0 
+        followStatus    = False
+        
+        # If follow button is pressed, then create a new Following object
+        if action == "follow" and not isFollowing:
+            followingObj = Following.objects.create(user=requestUser, followUser=userProfile)
+            numFollowers += 1
+            followStatus = True
+        # Else, unfollow the user
+        else:
+            following.delete()
+            numFollowers -= 1
+            followStatus = False
+
+    except:
+        return JsonResponse({"error": "Unable to follow/unfollow."}, status=404)
+    return JsonResponse({"numFollowers": numFollowers, "followStatus": followStatus}, status=201)
+
+
+def edit(request):
+    try:
+        data        = json.loads(request.body)
+        postContent = data.get("postContent")
+        postId      = data.get("postId")
+
+        post = Post.objects.filter(id=postId).first()
+        post.content = postContent
+        post.save()
+    except:
+        return JsonResponse({"error": "Could not save post."}, status=404)
+    return JsonResponse({"success": "Post updated."}, status=201)
+
